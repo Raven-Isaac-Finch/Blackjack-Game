282699d74ce168c0260a691b850c09efd7f411cb
Raven <purplebox02@gmail.com>
2022-06-04 13:02:57 +0300

    Bug Fixes & JS Updated

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-
-
-//************************************************************ */
-
 const computerBoard = document.getElementById('computer-cards');
 const playerBoard = document.getElementById('player-cards');
 
 const dealBtn = document.getElementById('hit-btn');
 const stayBtn = document.getElementById('stay-btn');
+const resetBtn = document.getElementById('reset-btn');
+const notifText = document.getElementById('notif-text');
 
 let playerPoint = 0;
 let computerPoint = 0;
 let checkCounter = 0;
+let computerAceCounter = 0;
+let playerAceCounter = 0;
+
+const dealClickFunction = () => {deal(playerBoard)};
+const stayClickFunction = () => {
+    dealBtn.disabled = true;
+    stayBtn.disabled = true;
+    if(computerPoint <= 15) {
+        deal(computerBoard);
+    } else if(computerPoint < playerPoint) {
+        deal(computerBoard);
+    }
+    theWinnerCheck();
+};
 
 function startGame() {
     deal(computerBoard);
@@ -18,10 +30,10 @@ function startGame() {
     deal(playerBoard);
     deal(playerBoard);
 
-    dealBtn.addEventListener('click', () => {deal(playerBoard)});
+      
 
-
-    
+    dealBtn.addEventListener('click', dealClickFunction);
+    stayBtn.addEventListener('click', stayClickFunction);    
 };
 
 function deal(forWho) {
@@ -38,12 +50,19 @@ function deal(forWho) {
             if(isNaN(cardValue) && cardValue !== "ACE" ) {
                 forWho == playerBoard ? playerPoint += 10 : computerPoint += 10;
             } else if(cardValue === "ACE" ) {
-                forWho == playerBoard ? playerPoint += 11 : computerPoint += 11;
+                if(forWho == playerBoard) {
+                    playerPoint += 11;
+                    playerAceCounter += 1;
+                } else if(forWho == computerBoard) {
+                    computerPoint += 11;
+                    computerAceCounter += 1;
+                }
             } else {
                 forWho == playerBoard ? playerPoint += parseInt(cardValue) : computerPoint += parseInt(cardValue);
             }
 
             blackjackCheck();
+            loseCheck();
 
             console.log(data.remaining);
             console.log(`playerPoint: ${playerPoint}`);
@@ -57,17 +76,83 @@ function shuffleDeck() {
 
 function blackjackCheck() {
     if(checkCounter === 4 && playerPoint === 21 && computerPoint !== 21) {
-        alert("You Won! You Have Blackjack!");
+        notifText.textContent = "You Won! You Have Blackjack!";
+        fixBtns();
     } else if(checkCounter === 4 && playerPoint !== 21 && computerPoint === 21) {
-        alert("You Lost! Computer Has Blackjack!");
+        notifText.textContent = "You Lost! Computer Has Blackjack!";
+        fixBtns();
+    } else if (checkCounter === 4 && playerPoint === 21 && computerPoint === 21) {
+        notifText.textContent = "It is a Natural Tie!";
+        fixBtns();
+    } else {
+        return;
     }
 }
 
 function loseCheck() {
-    if(playerPoint >= 21) {
-        alert("You Lost!");
+    if(playerPoint > 21) {
+        if(playerAceCounter > 0) {
+            playerPoint -= 10;
+            playerAceCounter -= 1;
+        } else {
+            notifText.textContent = "You Lost! You Hit over 21!";
+            fixBtns();
+        }
+    } else if(computerPoint > 21) {
+        if(computerAceCounter > 0) {
+            computerPoint -= 10;
+            computerAceCounter -= 1;
+        } else {
+            notifText.textContent = "You Won! The Computer Hit over 21!";
+            fixBtns();
+        }
+    } else {
+        return;
     }
 }
 
+function theWinnerCheck() {
+    if(computerPoint > playerPoint) {
+        notifText.textContent = "You lost! Computer Has More Points"
+        fixBtns();
+    } else if(playerPoint > computerPoint) {
+        notifText.textContent = "You lost! Computer Has More Points"
+        fixBtns();
+    } else if(playerPoint == computerPoint) {
+        notifText.textContent = "It is a Tie!";
+        fixBtns();
+    }
+}
+
+function clearGame() {
+    playerPoint = 0;
+    computerPoint = 0;
+    checkCounter = 0;
+    computerAceCounter = 0;
+    playerAceCounter = 0;
+    computerBoard.innerHTML = "";
+    playerBoard.innerHTML = "";
+    notifText.textContent = "";
+    resetBtn.disabled = true;
+    dealBtn.disabled = false;
+    dealBtn.removeEventListener('click', dealClickFunction);
+    stayBtn.disabled = false;
+    stayBtn.removeEventListener('click', stayClickFunction); 
+}
+
+function fixBtns() {
+    resetBtn.disabled = false;
+    dealBtn.disabled = true;
+    stayBtn.disabled = true;
+}
+
+resetBtn.addEventListener('click', () => {
+    clearGame();
+    shuffleDeck();
+    const gameTimeout = setTimeout(startGame, 500);
+    gameTimeout();
+});
+
 shuffleDeck();
 const gameTimeout = setTimeout(startGame, 500);
+gameTimeout();
